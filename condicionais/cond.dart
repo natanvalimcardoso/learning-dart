05db0814bcd327c2e0b83600878e5bc676e635c7
natanvalimcardoso <natanvalimcardoso@hotmail.com>
2021-07-13 18:49:50 -0300

    condi

---
+++ condicionais/cond.dart
@@ -0,0 +1,14 @@
+void main() {
+  print('Teste das condicional.');
+
+  int idade = 18;
+
+  //
+
+  if (idade >= 18) {
+    print('Você é maior de idade pode entrar');
+  }
+  else{
+    print('Você é menor de idade, não pode entrar!');
+  }
+}
---
+++ condicionais/cond2.dart
@@ -0,0 +1,13 @@
+void main() {
+  int idade = 290;
+
+  if (idade < 150) {
+    print('A pessoa é pequena');
+  } else if (idade >= 150 && idade < 175) {
+    print('A altura está na média!');
+  } else if (idade >= 175 && idade < 195) {
+    print('Você é grande!');
+  } else {
+    print('Você é enorme!!!');
+  }
+}
