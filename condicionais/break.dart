c96547b3e8dd81da2d011eedf68c994abf1088a8
natanvalimcardoso <natanvalimcardoso@hotmail.com>
2021-07-16 20:00:32 -0300

    new

---
+++ condicionais/break.dart
@@ -0,0 +1,13 @@
+void main() {
+  int contador = 0;
+
+  for (;;) {
+    contador++;
+    print('O valor do contador é $contador');
+
+    if (contador == 123) {
+      print('Atingi o valor 123');
+      break;
+    }
+  }
+}
---
+++ condicionais/switch.dart
@@ -0,0 +1,46 @@
+void main() {
+  int mes = 8;
+
+  switch(mes) {
+    case 1:
+      print("Janeiro.");
+      break;
+    case 2:
+      print("Fevereiro.");
+      break;
+    case 3:
+      print("Março.");
+      break;
+    case 4:
+      print("Abril.");
+      break;
+    case 5:
+      print("Maio.");
+      break;
+    case 6:
+      print("Junho.");
+      break;
+    case 7:
+      print("Julho.");
+      break;
+    case 8:
+      print("Agosto.");
+      break;
+    case 9:
+      print("Setembro.");
+      break;
+    case 10:
+      print("Outubro.");
+      break;
+    case 11:
+      print("Novembro.");
+      break;
+    case 12:
+      print("Dezembro.");
+      break;
+    default:
+      print("Este mês não existe!");
+      break;
+
+  }
+}
