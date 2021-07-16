d55beda3b438302810950cfed72b3075b23f8526
natanvalimcardoso <natanvalimcardoso@hotmail.com>
2021-07-15 21:44:32 -0300

    new

---
+++ condicionais/cond4_for.dart
@@ -0,0 +1,7 @@
+void main() {
+  //  for ( variavél ; condição ; o que vai fazer com essa variavel.)) ==  (Pode ser criada dentro do for, porem ela so vai existir dentro do for.)
+
+  for (int contador = 0; contador <= 10; contador++){
+    print('O contador é ${contador}');
+  }
+}
---
+++ condicionais/cond5tabua.dart
@@ -0,0 +1,11 @@
+void main() {
+  for (int multiplicando = 1; multiplicando <= 10; multiplicando++) {
+    print('Tabuada de $multiplicando');
+
+    for (int contador = 1; contador <= 10; contador++) {
+      print('$multiplicando * $contador = ${multiplicando * contador}');
+
+      ///Se tiver com chaves ele imprimi primeiro!!
+    }
+  }
+}
