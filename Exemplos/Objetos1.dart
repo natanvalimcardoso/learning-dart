3f9e0ff46e05db56580b04aad06709e0d69a1a55
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-07-20 18:52:00 -0300

    Update Objetos1.dart

--- Exemplos/Objetos1.dart
+++ Exemplos/Objetos1.dart
@@ -23,3 +23,28 @@ class ContaCorrente {
   int conta = 0; //Importante inicializar os as nossa variaveis com algum valor.
   double saldo = 30.0; //
 }
+
+/////////////////////////////
+
+void main() {
+  ContaCorrente conta1 = ContaCorrente();
+  ContaCorrente conta2 = ContaCorrente();
+
+  conta1.titular = "Natan";
+  conta1.agencia = 123;
+  conta1.conta = 1;
+
+  conta2.titular = "Natan";
+  conta2.agencia = 123;
+  conta2.conta = 1;
+
+  print(conta1.titular == conta2.titular);
+
+}
+
+class ContaCorrente {
+  String titular = ''; //
+  int agencia = 145; //
+  int conta = 0; //Importante inicializar os as nossa variaveis com algum valor.
+  double saldo = 30.0; //
+}
