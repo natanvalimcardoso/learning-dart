1e3a4d9bfca874377e95c31c69d574be98410b97
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-07-21 20:40:19 -0300

    Update read.dart

--- exercicios/read.dart
+++ exercicios/read.dart
@@ -4,3 +4,5 @@ void main() {
   stdout.write('Qual é o seu nome: '); // Escreva
   var nome = stdin.readLineSync();      //Leia
 }                                        //stdout.write com o ln é paragrafo exemplo: stdout.writeln
+
+// https://www.youtube.com/watch?v=Ej_Pcr4uC2Q&t=2725s  (21/07/2021)
