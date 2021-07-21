c1ec28426bbf15de832e60d797e2a947a4832f62
natanvalimcardoso <natanvalimcardoso@hotmail.com>
2021-07-21 20:38:56 -0300

    nova pasta

---
+++ exercicios/read.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Qual é o seu nome: '); // Escreva
+  var nome = stdin.readLineSync();      //Leia
+}                                        //stdout.write com o ln é paragrafo exemplo: stdout.writeln
