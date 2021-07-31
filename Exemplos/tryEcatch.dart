d88b6da5b5df54bfca03aae845e61435776b97d8
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-07-31 19:58:31 -0300

    finally

--- Exemplos/tryEcatch.dart
+++ Exemplos/tryEcatch.dart
@@ -6,5 +6,7 @@ void main() {
     print(num1 / num2);
   } catch (error) {              //Usado para evitar erros!! è um IF & ELSe para bugs.
     print('caiu no error');
+  }finally{
+    print("Erro aqui");     //Usado para todo comando ser executado sempre sem um try independente.
   }
 }
