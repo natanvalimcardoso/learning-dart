94c76542ba5313448855eb7b2808a3112465b400
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-08-02 18:07:30 -0300

    switch

---
+++ Exemplos/switch.dart
@@ -0,0 +1,21 @@
+void main (){
+  int x = 2;
+
+  switch(x){
+    case 0:
+      print('0');
+    break;
+    case 1:
+      print('1');
+    break;
+    case 2:
+      print('2');
+    break;
+    case 3:
+      print('3');
+    break;
+    case 4:
+      print('4');
+    break;
+  }
+}
