b286b23f6248cb426dd54865a2f637caab627f22
Natan Valim <natanvalimcardoso@rede.ulbra.br>
2021-10-05 18:20:36 -0300

    new

---
+++ novasFuncoes/OperadoresAtribui.dart
@@ -0,0 +1,20 @@
+void main() {
+  dynamic numero = 10;
+
+  numero = numero + 10;
+  numero += 10; 
+
+  numero = numero - 10;
+  numero -= 10; //melhor
+
+  numero = numero * 10;
+  numero *= 10; //melhor
+
+  numero = numero / 10;
+  numero /= 10; //melhor
+
+  numero = numero % 10;
+  numero %= 10; //melhor
+
+  print(numero);
+}
