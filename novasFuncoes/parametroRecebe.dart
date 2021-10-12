0f9acf1c67a3ad698f348575584bd2e988d9693c
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-10-12 16:37:12 -0300

    novo

--- novasFuncoes/parametroRecebe.dart
+++ novasFuncoes/parametroRecebe.dart
@@ -1,10 +1,20 @@
+import 'dart:math';
+
 void main() {
   int resultado = somar(2, 3);
+  int somador = somarAleatorio();
   resultado *= 2;
 
   print("o dobro do resultado é: $resultado");
+  print(somador);
 }
 
 int somar(int a, int b) {
+  return a + b;
+}
+
+int somarAleatorio() {
+  int a = Random().nextInt(11);
+  int b = Random().nextInt(11);
   return a + b;
 }
