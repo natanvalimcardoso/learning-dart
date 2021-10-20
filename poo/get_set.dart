09b38e40561f181ebbd0b5f03778a2fb6fc5bbe9
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-10-20 19:02:21 -0300

    get

---
+++ poo/get_set.dart
@@ -0,0 +1,10 @@
+class Bebe {           //Na questão de segurança caso eu queira que meu bebe não possa ter a idade altera por fora da classe colocamos um _ na frente do nome da váriavel
+  String _nome = "bebes";
+  int _idade = 0;
+  int _tamanho = 0;
+
+  Bebe(this._nome, this._idade, this._tamanho){
+    
+  }
+}
+
