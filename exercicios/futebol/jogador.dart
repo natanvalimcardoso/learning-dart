5291069138a34b6fffca561085fa924f19ce617b
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-12-28 18:59:24 -0300

    futebol

--- exercicios/futebol/jogador.dart
+++ exercicios/futebol/jogador.dart
@@ -1,3 +1,14 @@
 class Jogador {
-  
+  String nome = '';
+  String posicao = '';
+  int idade = 0;
+  String nacionalidade = '';
+  int altura = 0;
+  int peso = 0;
+
+  get mostrarDadosJogadores {}
+
+  get calcularIdade {}
+
+  get calcularAposentadoria {}
 }
