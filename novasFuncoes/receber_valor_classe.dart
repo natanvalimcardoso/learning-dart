9ac06ecc162b48e4f9c2fd0064ddbded4d87ef97
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-12-29 20:01:08 -0300

    parametros

--- novasFuncoes/receber_valor_classe.dart
+++ novasFuncoes/receber_valor_classe.dart
@@ -12,6 +12,11 @@ class Gente {
   String nome ='';
   int idade = 0;
   String sexo = '';
+  
+  String _nomeLocal = 'local';
+  final umaVez = 'deu';
 
   Gente({required String this.nome, required int this.idade, required String this.sexo});
+
+  
 }
