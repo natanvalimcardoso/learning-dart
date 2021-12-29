0d05b5a24facbb93d9dec4742a8dd49e0519369b
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-12-28 21:40:45 -0300

    new

--- exercicios/futebol/jogador.dart
+++ exercicios/futebol/jogador.dart
@@ -7,12 +7,11 @@ class Jogador {
   int altura = 0;
   int peso = 0;
 
-  get mostrarDadosJogadores {
-    print(
-        'Nome: $nome \n Posição: $posicao \n Idade: $idade \n Altura: $altura \n Peso: $peso');
+  mostrarDadosJogadores() {
+    print(' Nome: $nome \n Posição: $posicao \n Idade: $idade \n Altura: $altura \n Peso: $peso \n');
   }
 
-  get calcularAposentadoria {
+  calcularAposentadoria() {
     switch (posicao) {
       case 'atacante':
         if (idade >= 35) {
@@ -35,6 +34,23 @@ class Jogador {
           print('Pode jogar bola ainda!');
         }
     }
+  }
+
+  perguntas() {
+    int numero = 0;
+    do {
+      print("\nDigite 1 para mostrar o jogador:\nDigite 2 para testar idade: ");
+      var entrada = stdin.readLineSync();
+      numero = int.parse(entrada!);
+
+      if (numero == 1) {
+        mostrarDadosJogadores();
+      } else if (numero == 2) {
+        calcularAposentadoria();
+      } else {
+        print('Numero incorreto digite 1 ou 2 e para sair 99');
+      }
+    } while (numero != 99);
   }
 }
 
--- exercicios/futebol/main.dart
+++ exercicios/futebol/main.dart
@@ -2,25 +2,33 @@ import 'dart:io';
 import 'jogador.dart';
 
 main() {
-  List<String> listString = ['nome:', 'posicao:'];
-  var contator = 0;
+  List<String> listString = ['nome', 'posicao'];
+  List<String> listInt = ['idade', 'altura', 'peso'];
   var jogador = Jogador();
 
-  do {
-    if (contator <= 2) {
-      for (int cont = 0; cont <= listString.length; cont++) {
-        stdout.write("digite ${listString[cont]}: ");
-        var valor = stdin.readLineSync();
-
-        if ('nome:' == listString[cont]) {
-          jogador.nome = valor!;
-        } else {
-          jogador.posicao = valor!;
-        }
-      }
+  for (int cont = 0; cont < listString.length; cont++) {
+    stdout.write("digite ${listString[cont]}: ");
+    var valor = stdin.readLineSync();
+    if ('nome' == listString[cont]) {
+      jogador.nome = valor!;
     } else {
-      print('object');
+      jogador.posicao = valor!;
     }
-    contator++;
-  } while (contator != 5);
+  }
+  for (int cont = 0; cont < listInt.length; cont++) {
+    stdout.write("digite ${listInt[cont]}: ");
+    var entrada = stdin.readLineSync();
+    int numero = int.parse(entrada!);
+
+    if ('idade' == listInt[cont]) {
+      jogador.idade = numero;
+    }
+    if ('altura' == listInt[cont]) {
+      jogador.altura = numero;
+    }
+    if ('peso' == listInt[cont]) {
+      jogador.peso = numero;
+    }
+  }
+  jogador.perguntas();
 }
