efb31187fb51327bb71396dbd01c6b5b9af6f35a
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-02-10 19:51:15 -0300

    exercise - Elevador

---
+++ desafios/dasafio_elevador/main.dart
@@ -0,0 +1,13 @@
+/* 
+Crie uma classe Elevador para armazenar as informações de um elevador de prédio. A classe deve armazenar o andar atual (térreo = 0), total de andares no prédio (desconsiderando o térreo), capacidade do elevador e quantas pessoas estão presentes nele. A classe deve também disponibilizar os seguintes métodos:
+
+Inicializar: que deve receber como parâmetros a capacidade do elevador e o total de andares no prédio (os elevadores sempre começam no térreo e vazio);
+Entrar: para acrescentar uma pessoa no elevador (só deve acrescentar se ainda houver espaço);
+Sair: para remover uma pessoa do elevador (só deve remover se houver alguém dentro dele);
+Subir: para subir um andar (não deve subir se já estiver no último andar);
+Descer: para descer um andar (não deve descer se já estiver no térreo);
+Obs.: Encapsular todos os atributos da classe (criar os métodos set e get).
+ */
+main(){
+  
+}
