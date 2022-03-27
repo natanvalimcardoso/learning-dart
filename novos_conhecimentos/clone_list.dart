287ae9a0bba0df92d04afde94c8d298909dc5225
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-03-27 16:45:01 -0300

    clone-lista

---
+++ novos_conhecimentos/clone_list.dart
@@ -0,0 +1,8 @@
+// Há varias maneneiras de clonar uma lista, porém a mais elegante é a seguinte:
+//O destruction é a forma mais simples de clonar uma lista em Dart. inclusive essa mesma estrategia
+// pode ser usado em Maps e Sets.
+main(){
+  var listNumeros = [1, 2, 3];
+  var listClone = [...listNumeros];
+  print(listClone);
+}
