aeb1976a4652f10cc4742da6b5800b2c728db0ba
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-04-09 13:59:18 -0300

    ready

--- desafios/desafio_matriz/main.dart
+++ desafios/desafio_matriz/main.dart
@@ -1,3 +1,24 @@
-main(){
-  
+import 'pergunta.dart';
+
+main() {
+  List matriz = [];
+
+  print('Digite o primeiro valor');
+  int primeiroValor = pergunta(0);
+
+  print('Digite o segundo valor');
+  int segundoValor = pergunta(0);
+
+  for (var i = 0; i < 10; i++) {
+    List list = [];
+
+    for (var j = 0; j < 10; j++) {
+      list.add(j);
+    }
+    matriz.add(list);
+  }
+
+  print(matriz);
+  print(matriz[2][4]);
+
 }
