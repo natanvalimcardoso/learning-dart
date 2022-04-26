b1c1fc8fbba7733f320bc16178e0a4f14db47c46
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2022-04-26 18:38:39 -0300

    Create exerc_estruturas.dart

---
+++ cursos/exerc_normais/exerc_estruturas.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+ 
+void main() {
+  stdout.write('Salário Fixo => R\$ ');
+  double salarioFixo = double.parse(stdin.readLineSync());
+ 
+  stdout.write('Valor das Vendas => R\$ ');
+  double valorVendas = double.parse(stdin.readLineSync());
+ 
+  stdout.write('Percentual de Comissão => ');
+  double percentualComissao = double.parse(stdin.readLineSync()) / 100;
+ 
+  double salarioFinal = salarioFixo + percentualComissao * valorVendas;
+ 
+  print('Comissão => R\$ ' +
+      (percentualComissao * valorVendas).toStringAsFixed(2) +
+      '\nSalário Final => R\$ ' +
+      salarioFinal.toStringAsFixed(2));
+}
