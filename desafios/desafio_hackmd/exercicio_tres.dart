095ff4bb1c78e90b6fd16271a07c43bb975623af
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-04-30 15:50:40 -0300

    exercicio 3

--- desafios/desafio_hackmd/exercicio_tres.dart
+++ desafios/desafio_hackmd/exercicio_tres.dart
@@ -1,4 +1,8 @@
 // Pegue uma lista, digamos, por exemplo, esta:
-//   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+// lista = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 4];
 // e escreva um programa que imprima todos os elementos da lista que são menores que 5.
 
+main(){
+  List lista = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 4];
+  print(lista.where((element) => element < 5));
+}
