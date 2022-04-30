878c9382e4ca7dfe185cbcee7a6b23d537936cf0
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-04-30 14:55:52 -0300

    ready

--- cursos/exerc_normais/exerc_estruturas.dart
+++ cursos/exerc_normais/exerc_estruturas.dart
@@ -1,19 +1,19 @@
-import 'dart:io';
+// import 'dart:io';
  
-void main() {
-  stdout.write('Salário Fixo => R\$ ');
-  double salarioFixo = double.parse(stdin.readLineSync());
+// void main() {
+//   stdout.write('Salário Fixo => R\$ ');
+//   double salarioFixo = double.parse(stdin.readLineSync());
  
-  stdout.write('Valor das Vendas => R\$ ');
-  double valorVendas = double.parse(stdin.readLineSync());
+//   stdout.write('Valor das Vendas => R\$ ');
+//   double valorVendas = double.parse(stdin.readLineSync());
  
-  stdout.write('Percentual de Comissão => ');
-  double percentualComissao = double.parse(stdin.readLineSync()) / 100;
+//   stdout.write('Percentual de Comissão => ');
+//   double percentualComissao = double.parse(stdin.readLineSync()) / 100;
  
-  double salarioFinal = salarioFixo + percentualComissao * valorVendas;
+//   double salarioFinal = salarioFixo + percentualComissao * valorVendas;
  
-  print('Comissão => R\$ ' +
-      (percentualComissao * valorVendas).toStringAsFixed(2) +
-      '\nSalário Final => R\$ ' +
-      salarioFinal.toStringAsFixed(2));
-}
+//   print('Comissão => R\$ ' +
+//       (percentualComissao * valorVendas).toStringAsFixed(2) +
+//       '\nSalário Final => R\$ ' +
+//       salarioFinal.toStringAsFixed(2));
+// }
