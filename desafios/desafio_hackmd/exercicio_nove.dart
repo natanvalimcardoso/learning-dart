b2b007fa3f2fde3a8c6a3accfc3ed5a20febe158
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-05-09 08:57:33 -0300

    exercicio 9 iniciado

---
+++ desafios/desafio_hackmd/exercicio_nove.dart
@@ -0,0 +1,9 @@
+/*Gere um número aleatório entre 1 e 100. Peça ao usuário para adivinhar o número e 
+  diga se ele adivinhou muito baixo, muito alto ou exatamente certo.
+  
+  Acompanhe quantos palpites o usuário fez e, quando o jogo terminar, imprima isso.
+*/
+
+main(){
+  
+}
