3b95cf1430ae742ca2af4a1404961ce240e04136
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-05-16 10:36:10 -0300

    exercicio 11 terminado

--- desafios/desafio_hackmd/exercicio_onze.dart
+++ desafios/desafio_hackmd/exercicio_onze.dart
@@ -3,3 +3,23 @@
 a = [5, 10, 15, 20, 25]
 e cria uma nova lista apenas com o primeiro e o último elementos da lista fornecida.
  Para praticar, escreva este código dentro de uma função.*/
+
+main() {
+  List lista = [5, 10, 15, 20, 25];
+
+  var clone = clonaLista(lista);
+  var listaComPrimeirosUltimos = primeiroUltimoLista(clone);
+  print(listaComPrimeirosUltimos);
+}
+
+clonaLista(List lista) {
+  var listClone = [...lista];
+  return listClone;
+}
+
+primeiroUltimoLista(List lista) {
+  List novaLista = [];
+  novaLista.add(lista.first);
+  novaLista.add(lista.last);
+  return novaLista;
+}
