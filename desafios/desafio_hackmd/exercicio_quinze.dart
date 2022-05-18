5a8cf583edf666451f7bd7810cedd3a06840beb8
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-05-18 09:30:43 -0300

    feat: arrumando arquivos

---
+++ desafios/desafio_hackmd/exercicio_quinze.dart
@@ -0,0 +1,16 @@
+/*
+Escreva um gerador de senhas no Dart. Seja criativo com a forma como você gera senhas - 
+senhas fortes têm uma mistura de letras minúsculas, letras maiúsculas, números e símbolos. 
+As senhas devem ser aleatórias, gerando uma nova senha toda vez que o usuário solicitar 
+uma nova senha. Inclua seu código de tempo de execução em um método principal.
+
+Pergunte ao usuário o quão forte ele quer que sua senha seja. Para senhas fracas, 
+escolha uma ou duas palavras de uma lista.
+*/ 
+
+import 'metodo_read.dart';
+
+main(){
+print('Você deseja uma senha (media, forte, fraca)');
+String senha = pergunta('');
+}
