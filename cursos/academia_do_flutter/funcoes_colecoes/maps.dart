501d9c7a3d1fe628fabf624be72ea03aad755eb9
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-06-22 13:53:28 -0300

    finalizado

--- cursos/academia_do_flutter/funcoes_colecoes/maps.dart
+++ cursos/academia_do_flutter/funcoes_colecoes/maps.dart
@@ -6,18 +6,42 @@ main() {
   };
   var produtos = <String, String>{};
 
-
   produtos.putIfAbsent('nome', () => 'abacaxi');
   produtos.putIfAbsent('idade', () => '21');
   paciente.update('nome', (value) => 'diana');
 
   //* Não aceita assincrono
-  produtos.forEach((key, value) { // O foreach não aceita nada assincrono e pra resolver tem quer ser com ForIn
+  produtos.forEach((key, value) {
+    // O foreach não aceita nada assincrono e pra resolver tem quer ser com ForIn
     print('A chavé é: $key e o value: $value');
   });
 
-  //* Aceita assincrono 
+  //* Aceita assincrono
   for (var element in produtos.entries) {
     print('chave: ${element.key} e Valor: ${element.value}');
   }
+
+  for (var key in produtos.keys) {
+    print('Todas key: $key');
+  }
+
+  //* Mapa dentro de mapa
+  var novoMapaProduto = produtos.map((key, value) {
+    return MapEntry(key, value.toUpperCase());
+  });
+
+  //* Mapa com partes maiores
+  var mapa = <String, Object>{
+    'nome': 'natan',
+    'curso': [
+      {
+        'nome': 'academia do flutter',
+        'descriç': 'muito bom',
+      },
+      {
+        'nome': 'masterclass',
+        'descriç': 'muito bom',
+      }
+    ]
+  };
 }
