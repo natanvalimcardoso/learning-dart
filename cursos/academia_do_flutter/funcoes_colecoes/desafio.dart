a54f8d82c650f07848c2c335dd3982eaf346a19d
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-06-30 09:34:30 -0300

    primeira parte

--- cursos/academia_do_flutter/funcoes_colecoes/desafio.dart
+++ cursos/academia_do_flutter/funcoes_colecoes/desafio.dart
@@ -1,9 +1,32 @@
 final pessoa = [
   'Rodrigo Ramalho|23|Masculino',
   'João Pedro|33|Masculino',
-  'Maria|40|Feminino',
   'José|45|Masculino',
   'João|33|Masculino',
   'Maria|40|Feminino',
   'Diana|40|Feminino',
+  'Maria|40|Feminino',
+  'Diana|40|Feminino',
 ];
+
+void main() {
+  Set<String> listaSemDuplicatas = pessoa.toSet(); //* Removi as duplicatas
+  var novaLista = listaSemDuplicatas.toList();
+  var listaMasculinoNome = [];
+  var listaFemininoNome = [];
+
+  for (var element in novaLista) {
+    var listaPaciente = element.toLowerCase().split('|');
+    if (listaPaciente.contains('masculino')) {
+      listaMasculinoNome.add(listaPaciente[0]);
+    } else {
+      listaFemininoNome.add(listaPaciente[0]);
+    }
+  }
+  print('O Total de pacientes Masculino é: ${listaMasculinoNome.length}:\nNome dos pacientes são:');
+  listaMasculinoNome.forEach(print);
+
+  print('\nO Total de pacientes Feminino é: ${listaFemininoNome.length}:\nNome dos pacientes são:');
+  listaFemininoNome.forEach(print);
+
+}
