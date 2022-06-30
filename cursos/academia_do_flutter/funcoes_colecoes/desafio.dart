b110f030b65ad5a3f7fed9dfcbfa3c1800af88c8
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-06-30 09:57:19 -0300

    3 parte finalizada

--- cursos/academia_do_flutter/funcoes_colecoes/desafio.dart
+++ cursos/academia_do_flutter/funcoes_colecoes/desafio.dart
@@ -1,8 +1,8 @@
 final pessoa = [
   'Rodrigo Ramalho|23|Masculino',
-  'João Pedro|33|Masculino',
-  'José|45|Masculino',
-  'João|33|Masculino',
+  'João Pedro|3|Masculino',
+  'José|5|Masculino',
+  'João|18|Masculino',
   'Maria|40|Feminino',
   'Diana|40|Feminino',
   'Maria|40|Feminino',
@@ -14,6 +14,7 @@ void main() {
   var novaLista = listaSemDuplicatas.toList();
   var listaMasculinoNome = [];
   var listaFemininoNome = [];
+  var maiorIdadeNomes = [];
 
   for (var element in novaLista) {
     var listaPaciente = element.toLowerCase().split('|');
@@ -23,10 +24,22 @@ void main() {
       listaFemininoNome.add(listaPaciente[0]);
     }
   }
-  print('O Total de pacientes Masculino é: ${listaMasculinoNome.length}:\nNome dos pacientes são:');
-  listaMasculinoNome.forEach(print);
+  // print('O Total de pacientes Masculino é: ${listaMasculinoNome.length}:\nNome dos pacientes são:');
+  // listaMasculinoNome.forEach(print);
 
-  print('\nO Total de pacientes Feminino é: ${listaFemininoNome.length}:\nNome dos pacientes são:');
-  listaFemininoNome.forEach(print);
+  // print('\nO Total de pacientes Feminino é: ${listaFemininoNome.length}:\nNome dos pacientes são:');
+  // listaFemininoNome.forEach(print);
+
+  for (var element in novaLista) {
+    var listaPaciente = element.toLowerCase().split('|');
+    var listaConvertida = int.parse(listaPaciente[1]);
+    if(listaConvertida > 18){
+      maiorIdadeNomes.add(listaPaciente[0]);
+    }
+  }
+  print('As pessoas maiores de idades são:\n');
+  maiorIdadeNomes.forEach(print);
+
+
 
 }
