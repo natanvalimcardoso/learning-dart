f8358247594588faeb91004a67e405249b8defab
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-07-05 15:51:52 -0300

    const

---
+++ cursos/academia_do_flutter/poo/classes_const/casa.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Casa {
+ final String janela;
+ final String porta;
+
+  const Casa({required this.janela, required this.porta});
+}
---
+++ cursos/academia_do_flutter/poo/classes_const/main.dart
@@ -0,0 +1,5 @@
+import 'casa.dart';
+
+void main() {
+  var casa = const Casa(janela: 'madeira', porta: 'madeira');
+}
