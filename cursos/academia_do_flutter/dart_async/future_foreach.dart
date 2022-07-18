1bd7a1c7fb898e47bd5081e386adb4f9e9a57579
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-07-18 19:31:57 -0300

    async/await

--- cursos/academia_do_flutter/dart_async/future_foreach.dart
+++ cursos/academia_do_flutter/dart_async/future_foreach.dart
@@ -1,12 +1,23 @@
 import 'dart:ffi';
 
-void main() {
+Future<void> main() async {
+  var nomes = ['Natan', 'Diana', 'Ana', 'Joao'];
 
+  //*Forma de percorrer um lista assincrona, a cada 2 segundo um nome é printado
+  for (var nome in nomes) {
+    print(await saudacao(nome)); 
+  }
 
+  //*Caso eu queira printar somente quando todos nomes estiverem pronto utilizamos: Map + Future.wait
+  var nomesFuturo = nomes.map((nome) => saudacao(nome)).toList();
+  var saudacoes = await Future.wait(nomesFuturo);
+  print(saudacoes);
 }
 
+////////////////// Metodos /////////////////
+
 Future<String> saudacao(String nome) {
-  return Future.delayed(Duration(seconds: 3), () => 'Olá $nome');
+  return Future.delayed(Duration(seconds: 2), () => 'Olá $nome');
 }
 
 Future saudacaoNatan() async {
