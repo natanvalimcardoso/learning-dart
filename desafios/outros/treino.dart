69ad09b58c138b12ec608c981bd366acfcd02557
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-11-07 19:36:03 -0300

    where 1 concluido

---
+++ desafios/outros/treino.dart
@@ -0,0 +1,9 @@
+main(){
+  var lista = ['joão', 'maria', 'pedro', 'jose', 'marcos', 'Janaina', 'Luana'];
+
+  //* Desafio 1 - Imprimir a lista de nomes com a primeira letra J
+ var listaComNomesLetraJ = lista.where((nomes) => nomes.toLowerCase().startsWith('j'));
+ print(listaComNomesLetraJ);
+  //* Desafio 2 - Imprimir a lista de nomes com o tamanho maior que 5
+//  var listaComNomesMaior5 = lista.where((nomes) => false)
+}
