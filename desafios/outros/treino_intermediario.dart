1bed28dd4921d57b2d3e54d073b444bf58537957
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-11-27 21:06:12 -0300

    finish

--- desafios/outros/treino_intermediario.dart
+++ desafios/outros/treino_intermediario.dart
@@ -26,28 +26,37 @@ void main() {
   // var newListas = listPeople.where((p) => p.age > 30 && p.country == 'Brasil').toList();
   // newListas.forEach((person) => print(person.name));
   //* Descobrir a média da idade dos homens
-  var listaIdadeMasculino =
-      listPeople.where((person) => person.gender.toLowerCase() == 'masculino');
-  listaIdadeMasculino.forEach((personMasculino) {
-    mediaIdadeHomens += personMasculino.age;
-  });
+  // var listaIdadeMasculino =
+  //     listPeople.where((person) => person.gender.toLowerCase() == 'masculino');
+  // listaIdadeMasculino.forEach((personMasculino) {
+  //   mediaIdadeHomens += personMasculino.age;
+  // });
 
-  print(mediaIdadeHomens / listaIdadeMasculino.length);
+  // print(mediaIdadeHomens / listaIdadeMasculino.length);
 
   //* Descobrir o total de pessoas no Brasil. E se há mais mulheres ou homens printando o maior;
 
-  var listaPessoasBrasil = listPeople.where((person) => person.country.toLowerCase() == 'brasil');
-  listaPessoasBrasil.forEach((person) {
-      if (person.gender.toLowerCase() == 'feminino') {
-        qntMulherer++;
-      } else {
-        qntHomens++;
-      }
-    },
-  );
-  qntMulherer > qntHomens
-      ? print('A quantidade total é: ${listaPessoasBrasil.length} e o número de mulheres é maior: $qntMulherer')
-      : print('A quantidade total é: ${listaPessoasBrasil.length} e o número de homens é maior: $qntHomens');
+  // var listaPessoasBrasil = listPeople.where((person) => person.country.toLowerCase() == 'brasil');
+  // listaPessoasBrasil.forEach(
+  //   (person) {
+  //     if (person.gender.toLowerCase() == 'feminino') {
+  //       qntMulherer++;
+  //     } else {
+  //       qntHomens++;
+  //     }
+  //   },
+  // );
+  // qntMulherer > qntHomens
+  //     ? print(
+  //         'A quantidade total é: ${listaPessoasBrasil.length} e o número de mulheres é maior: $qntMulherer')
+  //     : print(
+  //         'A quantidade total é: ${listaPessoasBrasil.length} e o número de homens é maior: $qntHomens');
 
   //* Descobrir a quantidade de pessoas com mais de 30 anos e do Brasil
+  var listaPessoasCom30Brasil = listPeople.where(
+    (nomes) => nomes.country.toLowerCase() == 'brasil' && nomes.age > 30,
+  );
+  for (var nomes in listaPessoasCom30Brasil) {
+    print(nomes.name);
+  }
 }
