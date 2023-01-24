ce44c7a344585f6501ec4764eab7556042514250
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2023-01-24 16:04:51 -0300

    ready

--- testes/listas.dart
+++ testes/listas.dart
@@ -1,12 +1,38 @@
-class CategoryModel{
+class CategorysModel {
   final String? iconPath;
   final String? segmentId;
   final String? name;
   final String? segmentName;
 
-  CategoryModel(this.iconPath, this.segmentId, this.name, this.segmentName);
+  CategorysModel({this.iconPath, this.segmentId, this.name, this.segmentName});
+}
+
+class CampaingsModel {
+  final String? title;
+  final List<CategorysModel>? categories;
+
+  CampaingsModel({this.title, this.categories});
 }
 
 void main(List<String> args) {
-  List 
+  List listaCampaings = [
+    CampaingsModel(title: 'natan', categories: [
+      CategorysModel(
+        iconPath: 'natan',
+        segmentId: 'natan',
+        name: 'natan',
+        segmentName: 'natan',
+      ),
+    ]),
+    CampaingsModel(title: 'natan1', categories: [
+      CategorysModel(
+        iconPath: 'natan1',
+        segmentId: 'natan1',
+        name: 'natan1',
+        segmentName: 'natan1',
+      ),
+    ])
+  ];
+
+  
 }
