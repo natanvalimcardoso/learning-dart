74e3e3722e0a2c7075218ec712a6a0ddeaebd9f1
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2023-01-24 15:31:12 -0300

    ready

---
+++ testes/listas.dart
@@ -0,0 +1,12 @@
+class CategoryModel{
+  final String? iconPath;
+  final String? segmentId;
+  final String? name;
+  final String? segmentName;
+
+  CategoryModel(this.iconPath, this.segmentId, this.name, this.segmentName);
+}
+
+void main(List<String> args) {
+  List 
+}
