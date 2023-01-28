9f0b573446b5d904929838243b8a0a47494400d2
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2023-01-28 16:42:41 -0300

    finish

--- testes/other_lista.dart
+++ testes/other_lista.dart
@@ -1,3 +1,9 @@
 void main(List<String> args) {
-  
+  List lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List lista2 = [];
+
+  for (var element in lista) {
+     lista2.add(element);
+  }
+  print(lista2);
 }
