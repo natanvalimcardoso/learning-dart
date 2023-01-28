1491af16ddb7992ef43aeba8828374ad9c090738
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2023-01-28 17:44:28 -0300

    finish

--- testes/other_lista.dart
+++ testes/other_lista.dart
@@ -1,9 +1,45 @@
-void main(List<String> args) {
-  List lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  List lista2 = [];
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Pessoa {
+  String nome;
+  int idade;
 
+  Pessoa({
+    required this.nome,
+    required this.idade,
+  });
+}
+
+class Profissao {
+  int salario;
+  List<Pessoa> pessoas;
+
+  Profissao({
+    required this.salario,
+    required this.pessoas,
+  });
+}
+
+void main(List<String> args) {
+  List<Profissao> lista = List.generate(
+      10,
+      (index) => Profissao(
+            salario: index * 1000,
+            pessoas: List.generate(
+                10,
+                (index) => Pessoa(
+                      nome: 'Pessoa $index',
+                      idade: index * 10,
+                    )),
+          ));
+
+  var listaSalario = [];
+
+  // listaSalario.addAll(lista.map((element) => element.salario)); // retorna salario
   for (var element in lista) {
-     lista2.add(element);
+    listaSalario.add(element.salario);
   }
-  print(lista2);
+
+  
+
+  print(listaSalario);
 }
